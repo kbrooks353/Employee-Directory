b3180e0b52ca4e18cf73f3387e6afb46b2677bff
Kyle <kbrooks353@gmail.com>
2019-11-15 12:19:14 -0800

    search by name now works + sort by age also works

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -3,30 +3,66 @@ import API from "./utils/API"
 import EmployeeCard from './components/Card/Card';
 import Wrapper from "../src/components/Wrapper/index";
 import SearchForm from "../src/components/SearchForm/Search";
-
+import Sort from "../src/components/SortButton/Sort";
 
 
 class App extends Component {
   state = {
-    users: []
+    users: [],
+    search: "",
+    searchedUsers: []
   };
 
   componentDidMount() {
     API.getRandomEmployees().then(results => {
       this.setState({
         users: results.data.results,
+        searchedUsers: results.data.results
       });
     });
-    console.log(this.state.users)
+  };
+
+  handleAgeSort = event => {
+    let users = this.state.users;
+    users.sort(function(a,b){
+      return a.dob.age-b.dob.age;
+    })
+    this.setState({
+      searchedUsers: users
+    })
+  }
+
+  handleInputChange = event => {
+    this.setState({
+      search: event.target.value
+    });
+  }
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.search);
+    let search = this.state.search.toLowerCase();
+
+    let filterUsers = this.state.users.filter(item => {
+      return item.name.first.toLowerCase().includes(search);
+      
+    })
+    this.setState({
+      searchedUsers: filterUsers
+    })
   }
 
   render () {
     return (
       <div>
-        <SearchForm />
+        <SearchForm 
+          formSubmit={this.handleFormSubmit}
+          inputChange={this.handleInputChange}
+          />
+        <Sort handleSort={this.handleAgeSort}/>
         <Wrapper>
           <React.Fragment>
-          {this.state.users.map(user => (
+          {this.state.searchedUsers.map(user => (
             <EmployeeCard 
               first={user.name.first}
               last={user.name.last}
--- employee-directory/src/components/SearchForm/Search.js
+++ employee-directory/src/components/SearchForm/Search.js
@@ -2,14 +2,19 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
-function SearchForm(){
+
+function SearchForm(props){
     return(
-        <Form>
+        <Form onSubmit={props.formSubmit}>
             <Form.Group as={Row} controlId="exampleForm.ControlInput1">
                 <Col className="col-sm-8 offset-2">
                     <Form.Label>Search by Name</Form.Label>
-                    <Form.Control as="input"/>
+                    <Form.Control as="input" onChange={props.inputChange}/>
+                    <Button className="col-sm-2 offset-5" variant="primary" type="submit">
+                    Submit
+                    </Button>
                 </Col>
             </Form.Group>
         </Form>
